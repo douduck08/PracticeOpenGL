e8450f0a94e2179da10924a152ee1d60116ae549
douduck08 <lu10082000@gmail.com>
2017-09-22 16:35:45 +0800

    init project

---
+++ PracticeOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PracticeOpenGL
+//
+//  Created by 呂宗翰 on 2017/9/22.
+//  Copyright © 2017年 Leo Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
